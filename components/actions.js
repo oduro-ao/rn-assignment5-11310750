43f30f663cd6a4a58c0631af0632cffdf1783e8b
oduro-ao <oadu-owusu@st.ug.edu.gh>
2024-06-26 23:51:27 +0000

    second commit

--- components/actions.js
+++ components/actions.js
@@ -1,19 +1,21 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 
 const icons = [
-  require("../assets/icons/send.png"),
-  require("../assets/icons/recieve.png"),
-  require("../assets/icons/loan.png"),
-  require("../assets/icons/topUp.png"),
+  { icon: require("../assets/icons/send.png"), text: "Sent" },
+  { icon: require("../assets/icons/recieve.png"), text: "Recieved" },
+  { icon: require("../assets/icons/loan.png"), text: "loan" },
+  { icon: require("../assets/icons/topUp.png"), text: "TopUp" },
 ];
 
 export default function actions() {
-  
   return (
     <View style={styles.container}>
       {icons.map((icons) => (
-        <View style={styles.action}>
-          <Image source={icons} />
+        <View style={styles.container1}>
+          <View style={styles.action}>
+            <Image source={icons.icon} />
+          </View>
+          <Text style={styles.icon_text}>{icons.text}</Text>
         </View>
       ))}
     </View>
@@ -26,12 +28,20 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginTop: 30,
   },
+  container1: {
+    alignItems: "center",
+    justifyContent: "center"
+  }, 
   action: {
     backgroundColor: "#9898981f",
     width: 60,
     height: 60,
     borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  icon_text: {
+    marginTop: 5,
+    fontSize: 15,
   },
 });
